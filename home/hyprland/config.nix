32ffe7228f183e6ca3caf315ce59f7591f8f3ba3
Derek <zedikdagod@gmail.com>
2024-11-22 16:38:17 -0800

    fix hyprland monitors

--- home/hyprland/config.nix
+++ home/hyprland/config.nix
@@ -301,8 +301,8 @@
     };
 
     extraConfig = "
-      monitor=DVI-I-1, 2560x1440@144, 0x0, 1
-      monitor=eDP-1, 2560x1440@60, 2560x1120, 2
+      monitor=DVI-I-1, 2560x1440@144.00000, 0x0, 1
+      monitor=eDP-1, 2560x1440@60.00000, 2560x1120, 2
 
       xwayland {
         force_zero_scaling = true
