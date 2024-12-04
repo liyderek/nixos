afdb9b2366202fca01cd781b0d349a4811a62ecf
Derek <zedikdagod@gmail.com>
2024-12-03 17:33:28 -0800

    fix systemd

--- configuration.nix
+++ configuration.nix
@@ -57,12 +57,12 @@
     initrd.verbose = false;
     kernelParams = [
       "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
+      # "splash"
+      # "boot.shell_on_fail"
+      # "loglevel=3"
+      # "rd.systemd.show_status=false"
+      # "rd.udev.log_level=3"
+      # "udev.log_priority=3"
     ];
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key
