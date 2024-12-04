4a2fd89139b725a7fabab298ccd25a4cf4abf2ae
Derek <zedikdagod@gmail.com>
2024-12-03 17:18:03 -0800

    boot splash screen

--- configuration.nix
+++ configuration.nix
@@ -39,6 +39,34 @@
       '';
       catppuccin.enable = true;
     };
+
+    plymouth = {
+      enable = true;
+      theme = "rings";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "rings" ];
+        })
+      ];
+    };
+
+    # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    # loader.timeout = 0;
   };
   # Use the systemd-boot EFI boot loader.
   # boot.loader.systemd-boot.enable = true;
