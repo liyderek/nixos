e6f6a8f5ebced6bfd3ff2a17cb7994837beda53e
Derek <zedikdagod@gmail.com>
2024-12-08 13:05:08 -0800

    test

--- home/emacs/doom.d/init.el
+++ home/emacs/doom.d/init.el
@@ -14,8 +14,35 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(doom!
-        :ui
+(doom! :completion
+       vertico
+
+       :ui
        doom
        ;; doom-dashboard
-       )
+       modeline
+       nav-flash
+       ophints
+       (popup +defaults)
+       window-select
+
+       :editor
+       evil
+
+       :emacs
+       undo
+
+       :term
+       eshell
+       vterm
+
+       :os
+       (:if (featurep :system 'macos) macos)
+       (tty +osc)
+
+       :lang
+       emacs-lisp
+       (nix +lsp)
+
+       :config
+       (default +bindings +smartparens))
