4e061f27129a789255d999986056ace0e88223a6
Derek <zedikdagod@gmail.com>
2024-12-07 22:12:01 -0800

    fix legcord

--- home/hyprland/config.nix
+++ home/hyprland/config.nix
@@ -298,7 +298,7 @@
         "float,title:^(branchdialog)$"
         "float,title:^(Confirm to replace files)$"
         "float,title:^(File Operation Progress)$"
-        "float,title:^(Legcord)$"
+        "float,title:^(legcord)$"
 
         "opacity 0.0 override,class:^(xwaylandvideobridge)$"
         "noanim,class:^(xwaylandvideobridge)$"
