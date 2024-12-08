3756e8945fb87d0767e85c323df9ca19de4d3bff
Derek <zedikdagod@gmail.com>
2024-12-07 20:42:43 -0800

    add zeditor config

--- home/misc/zeditor.nix
+++ home/misc/zeditor.nix
@@ -2,11 +2,23 @@
 {
   programs.zed-editor = {
     enable = true;
-    # extensions = [
-    #   "nix"
-    #   "toml"
-    #   "elixir"
-    #   "make"
-    # ];
+    extensions = [
+      "nix"
+      "toml"
+      "elixir"
+      "make"
+      "catppuccin"
+    ];
+    userSettings = {
+      vim_mode = true;
+      base_keymap = "VSCode";
+      ui_font_size = 16;
+      buffer_font_size = 16;
+      theme = {
+        mode = "system";
+        light = "One Light";
+        dark = "Catppuccin Mocha";
+      };
+    };
   };
 }
