69c0c162ca6cf3cb294200e72fd97108c9f5a37b
Derek <zedikdagod@gmail.com>
2024-12-08 11:42:12 -0800

    test

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
           modules = [
             ./configuration.nix
             catppuccin.nixosModules.catppuccin
-            inputs.nix-doom-emacs-unstraightened.hmModule
+            # inputs.nix-doom-emacs-unstraightened.hmModule
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
