7b714660883f0e84a8d0fad46dace9d0c19323a9
Derek <zedikdagod@gmail.com>
2024-12-17 18:57:39 -0800

    add nvim and kickstart.nvim

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,22 +1,8 @@
 { pkgs, inputs, ... }:
-let
-  nixvim = import (
-    builtins.fetchGit {
-      url = "https://github.com/nix-community/nixvim";
-      # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-      ref = "main";
-      rev = "4f1fe403b18c45614d6b81423038a34cff371244";
-    }
-  );
-in
 {
-  imports = [
-    nixvim.homeManagerModules.nixvim
+  home.packages = with pkgs; [
+    neovim
   ];
 
-  programs.nixvim = {
-    enable = true;
-    colorschemes.catppuccin.enable = true;
-    plugins.lualine.enable = true;
-  };
+  xdg.configFile.nvim.source = ./nvim;
 }
