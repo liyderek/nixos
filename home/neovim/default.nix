b75991e6922fc182d73c0fba0a5a8b8f8dda3e90
Derek <zedikdagod@gmail.com>
2024-12-17 18:48:47 -0800

    rev

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,7 +1,17 @@
 { pkgs, inputs, ... }:
+let
+  nixvim = import (
+    builtins.fetchGit {
+      url = "https://github.com/nix-community/nixvim";
+      # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+      ref = "main";
+      rev = "4f1fe403b18c45614d6b81423038a34cff371244";
+    }
+  );
+in
 {
   imports = [
-    inputs.nixvim.homeManagerModules.nixvim
+    nixvim.homeManagerModules.nixvim
   ];
 
   programs.nixvim = {
