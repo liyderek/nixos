8a16a4eb14080a42903d3e18b377717573fd8875
Derek <zedikdagod@gmail.com>
2024-12-17 21:53:21 -0800

    fix EVERYTHING

--- home/neovim/config/lua/rafi/plugins/debugger.lua
+++ home/neovim/config/lua/rafi/plugins/debugger.lua
@@ -6,51 +6,42 @@ return {
 			local dap = require('dap')
 			require('dapui').setup()
 
-			require('mason-nvim-dap').setup({
-				ensure_installed = {},
-				automatic_installation = false,
-				handlers = {
-					function(config)
-						print('i like men')
-						require('mason-nvim-dap').default_setup(config)
-						dap.adapters.lldb = {
-							type = 'executable',
-							command = '/etc/profiles/per-user/derek/bin/lldb',
-							name = 'lldb',
-							host = '127.0.0.1',
-							port = 12345,
-						}
-						dap.adapters.codelldb = {
-							type = 'server',
-							port = '12345',
-							executable = {
-								command = '/home/derek/.vscode-oss/extensions/vadimcn.vscode-lldb/adapter/codelldb',
-								args = { '--port', '12345' },
-							},
-						}
-						dap.configurations.cpp = {
-							{
-								name = 'Launch file',
-								type = 'codelldb',
-								request = 'launch',
-								program = function()
-									local path
-									vim.ui.input({
-										prompt = 'Path to executable: ',
-										default = vim.fn.getcwd() .. '/build/',
-									}, function(input)
-										path = input
-									end)
-									vim.cmd([[redraw]])
-									return path
-								end,
-								cwd = '${workspaceFolder}',
-								stopOnEntry = false,
-							},
-						}
-					end,
+			print('i like men')
+			dap.adapters.lldb = {
+				type = 'executable',
+				command = '/etc/profiles/per-user/derek/bin/lldb',
+				name = 'lldb',
+				host = '127.0.0.1',
+				port = 12345,
+			}
+			dap.adapters.codelldb = {
+				type = 'server',
+				port = '12345',
+				executable = {
+					command = '/home/derek/.vscode-oss/extensions/vadimcn.vscode-lldb/adapter/codelldb',
+					args = { '--port', '12345' },
 				},
-			})
+			}
+			dap.configurations.cpp = {
+				{
+					name = 'Launch file',
+					type = 'codelldb',
+					request = 'launch',
+					program = function()
+						local path
+						vim.ui.input({
+							prompt = 'Path to executable: ',
+							default = vim.fn.getcwd() .. '/build/',
+						}, function(input)
+							path = input
+						end)
+						vim.cmd([[redraw]])
+						return path
+					end,
+					cwd = '${workspaceFolder}',
+					stopOnEntry = false,
+				},
+			}
 		end,
 	},
 }
