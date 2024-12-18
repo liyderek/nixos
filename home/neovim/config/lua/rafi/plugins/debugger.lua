5d9bba1d0c515df34b9153363601a91e3e43ef0d
Derek <zedikdagod@gmail.com>
2024-12-17 21:03:23 -0800

    dap config

--- home/neovim/config/lua/rafi/plugins/debugger.lua
+++ home/neovim/config/lua/rafi/plugins/debugger.lua
@@ -1,41 +1,52 @@
-local dap = require("dap")
-require("dapui").setup()
-
-require("mason-nvim-dap").setup({
-	ensure_installed = {},
-	automatic_installation = false,
-	handlers = {
-		function(config)
-			require("mason-nvim-dap").default_setup(config)
-		end,
-		codelldb = function(source_name)
-			dap.configurations.cpp = {
-				{
-					name = "Launch file",
-					type = "codelldb",
-					request = "launch",
-					program = function()
-						local path
-						vim.ui.input(
-							{
-								prompt = "Path to executable: ",
-								default = vim.fn.getcwd() ..
-										"/build/"
-							}, function(input)
-								path = input
-							end)
-						vim.cmd [[redraw]]
-						return path
-					end,
-					cwd = "${workspaceFolder}",
-					stopOnEntry = false,
-				},
-			}
-		end,
-	}
-})
-
 return {
 	"mfussenegger/nvim-dap",
-	"jay-babu/mason-nvim-dap.nvim",
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		config = function()
+			local dap = require("dap")
+			require("dapui").setup()
+
+			require("mason-nvim-dap").setup({
+				ensure_installed = {},
+				automatic_installation = false,
+				handlers = {
+					function(config)
+						require("mason-nvim-dap").default_setup(config)
+					end,
+					codelldb = function(source_name)
+						dap.adapters.codelldb = {
+							type = "server",
+							port = "12345",
+							executable = {
+								command = "codelldb",
+								args = { "--port", "12345" }
+							}
+						}
+						dap.configurations.cpp = {
+							{
+								name = "Launch file",
+								type = "codelldb",
+								request = "launch",
+								program = function()
+									local path
+									vim.ui.input(
+										{
+											prompt = "Path to executable: ",
+											default = vim.fn.getcwd() ..
+													"/build/"
+										}, function(input)
+											path = input
+										end)
+									vim.cmd [[redraw]]
+									return path
+								end,
+								cwd = "${workspaceFolder}",
+								stopOnEntry = false,
+							},
+						}
+					end,
+				}
+			})
+		end
+	}
 }
