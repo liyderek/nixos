316e46a3a1ff57628bbb7a8014f127913f20ac2b
Derek <derek@derek.com>
2025-02-01 13:17:12 -0800

    neovide

--- home/neovim/config/init.lua
+++ home/neovim/config/init.lua
@@ -9,6 +9,8 @@ local lazy_override = stdconfig .. '/lua/config/lazy.lua'
 if vim.g.neovide then
 	vim.o.guifont = 'SFMono Nerd Font:h12'
 	vim.keymap.set('i', '<C-V>', '"+p')
+	vim.g.neovide_cursor_animation_length = 0
+	vim.g.neovide_scroll_animation_length = 0
 end
 
 vim.uv = vim.uv or vim.loop
