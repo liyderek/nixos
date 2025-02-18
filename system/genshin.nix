b192c5b5c22a86c6853159ef6603ff91a93412b5
Derek <derek@derek.com>
2025-02-18 09:49:10 -0800

    genshin

--- system/genshin.nix
+++ system/genshin.nix
@@ -10,10 +10,5 @@ in
   ];
 
   nix.settings = aagl.nixConfig;
-  programs.anime-game-launcher.enable = true;
-  programs.anime-games-launcher.enable = true;
-  programs.honkers-railway-launcher.enable = true;
-  programs.honkers-launcher.enable = true;
-  programs.wavey-launcher.enable = true;
-  programs.sleepy-launcher.enable = true;
+  programs.an-anime-game-launcher.enable = true;
 }
