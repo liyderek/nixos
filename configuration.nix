b6fa7c3be75be7a62e150006a50052677f37a9eb
Derek <derek@derek.com>
2025-03-14 22:33:48 -0700

    fix vpn

--- configuration.nix
+++ configuration.nix
@@ -186,6 +186,25 @@
 
   programs.zsh.enable = true;
 
+  # vpn
+  services.mullvad-vpn.enable = true;
+  services.mullvad-vpn.package = pkgs.mullvad-vpn;
+  networking.nameservers = [
+    "1.1.1.1#one.one.one.one"
+    "1.0.0.1#one.one.one.one"
+  ];
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [
+      "1.1.1.1#one.one.one.one"
+      "1.0.0.1#one.one.one.one"
+    ];
+    dnsovertls = "true";
+  };
+
   # nix ld
   programs.nix-ld = {
     enable = true;
@@ -230,7 +249,6 @@
     mkosi
     ripgrep
     ntfs3g
-    mullvad-vpn
   ];
 
   system.stateVersion = "24.11"; # dont change
