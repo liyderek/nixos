b7d20caab94396345dbe9d74ea898bd1f6fe5812
Derek <derek@derek.com>
2025-04-05 23:29:21 -0700

    fix

--- configuration.nix
+++ configuration.nix
@@ -226,6 +226,33 @@
     ];
   };
 
+  security.wrappers.sunshine = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_sys_admin+p";
+    source = "${pkgs.sunshine}/bin/sunshine";
+  };
+
+  services.avahi.publish.enable = true;
+  services.avahi.publish.userServices = true;
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [
+      47984
+      47989
+      47990
+      48010
+    ];
+    allowedUDPPortRanges = [
+      {
+        from = 47998;
+        to = 48000;
+      }
+      #{ from = 8000; to = 8010; }
+    ];
+  };
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.derek = {
     isNormalUser = true;
