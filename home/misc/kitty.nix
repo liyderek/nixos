c387a4cb1531fd742d9628cf8b978eb64b52662d
Derek <derek@derek.com>
2025-05-13 16:37:24 -0700

    fix kitty

--- home/misc/kitty.nix
+++ home/misc/kitty.nix
@@ -19,7 +19,7 @@
 
     settings = {
       confirm_os_window_close = 0;
-      background_opacity = "0.66";
+      background_opacity = "1.0";
       scrollback_lines = 10000;
       enable_audio_bell = false;
       mouse_hide_wait = 60;
