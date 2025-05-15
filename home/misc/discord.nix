840c3ac702420da25db41f551e8d9f9281c1e693
Derek <derek@derek.com>
2025-05-15 16:31:56 -0700

    fix discord

--- home/misc/discord.nix
+++ home/misc/discord.nix
@@ -6,7 +6,4 @@
       withVencord = true;
     })
   ];
-  xdg.configFile."Vencord/themes/custom.css".text = ''
-    @import url("https://gist.githubusercontent.com/emctague/aab1d43a90174930939ae51c8db09553/raw/7c1ed130bb7d053060019ae0a73d9859bb525613/bd-gruvbox.css");
-  '';
 }
