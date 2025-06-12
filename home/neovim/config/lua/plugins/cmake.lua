cfd87a025edcfebc2461ac56e8109fe0f85414ef
Derek <derek@derek.com>
2025-06-11 21:35:49 -0700

    fix tmux

--- home/neovim/config/lua/plugins/cmake.lua
+++ home/neovim/config/lua/plugins/cmake.lua
@@ -7,6 +7,11 @@ return {
 			cmake_compile_commands_options = {
 				action = 'lsp',
 			},
+			configure_args = { '-DCMAKE_EXPORT_COMPILE_COMMANDS=1' },
+			on_configure_done = function()
+				vim.lsp.stop_client(vim.lsp.get_active_clients())
+				vim.cmd('edit') -- reload file to restart LSP with updated flags
+			end,
 		})
 	end,
 }
