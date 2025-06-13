179747865cab8e4a03c2687014dadc2cb1cc2b4d
Derek <derek@derek.com>
2025-06-12 19:52:09 -0700

    fix debugger

--- home/neovim/config/lua/plugins/debugger.lua
+++ home/neovim/config/lua/plugins/debugger.lua
@@ -5,9 +5,6 @@ local allowed_projects = {
 	'/home/derek/Documents/codeforces/',
 }
 
-local cwd = vim.fn.getcwd()
-local is_allowed = vim.tbl_contains(allowed_projects, cwd)
-
 -- dap.adapters.lldb = {
 -- 	type = 'executable',
 -- 	command = '/etc/profiles/per-user/derek/bin/lldb',
@@ -15,8 +12,10 @@ local is_allowed = vim.tbl_contains(allowed_projects, cwd)
 -- 	host = '127.0.0.1',
 -- 	port = 12345,
 -- }
-if is_allowed then
-	dap.adapters.codelldb = function(cb, config)
+dap.adapters.codelldb = function(cb, config)
+	local cwd = vim.fn.getcwd()
+	local is_allowed = vim.tbl_contains(allowed_projects, cwd)
+	if is_allowed then
 		local command = {
 			'/run/current-system/sw/bin/g++',
 			'-g',
@@ -29,29 +28,29 @@ if is_allowed then
 		local output = vim.system(command):wait()
 		print('stdout: ' .. output.stdout)
 		print('stderr: ' .. output.stderr)
-
-		cb({
-			type = 'server',
-			port = '12345',
-			executable = {
-				command = '/home/derek/.vscode-oss/extensions/vadimcn.vscode-lldb/adapter/codelldb',
-				args = { '--port', '12345' },
-			},
-		})
 	end
-	dap.configurations.cpp = {
-		{
-			name = 'Launch file',
-			type = 'codelldb',
-			request = 'launch',
-			program = function()
-				return vim.fn.getcwd() .. '/main'
-			end,
-			cwd = '${workspaceFolder}',
-			stopOnEntry = false,
+
+	cb({
+		type = 'server',
+		port = '12345',
+		executable = {
+			command = '/home/derek/.vscode-oss/extensions/vadimcn.vscode-lldb/adapter/codelldb',
+			args = { '--port', '12345' },
 		},
-	}
+	})
 end
+dap.configurations.cpp = {
+	{
+		name = 'Launch file',
+		type = 'codelldb',
+		request = 'launch',
+		program = function()
+			return vim.fn.getcwd() .. '/main'
+		end,
+		cwd = '${workspaceFolder}',
+		stopOnEntry = false,
+	},
+}
 
 return {
 	{
